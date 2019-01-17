f9ebebce7baa9eae0741de1b9a5581df0ba55a0f
5ia17jokoglin <josua.koglin@bluewin.ch>
2019-01-17 19:59:06 +0100

    Implementing subtraction-method into calculator-class

--- src/main/java/ch/bbw/Calculator.java
+++ src/main/java/ch/bbw/Calculator.java
@@ -1,9 +1,13 @@
 package ch.bbw;
 
 public class Calculator {
+
   public int sum(int summand1, int summand2) {
     return summand1 + summand2;
   }
 
+  public int subtraction(int value1, int value2) {
+    return value1 - value2;
+  }
 
 }
