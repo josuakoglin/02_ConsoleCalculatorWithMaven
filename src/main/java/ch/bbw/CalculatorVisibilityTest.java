74cee8ed519b7bc7d2de22ae1b54b10f9aa43ead
5ia17jokoglin <josua.koglin@bluewin.ch>
2019-01-20 12:40:49 +0100

    Implement more tests into Class: CalculatorVisibilityTest for multiplication-method

--- src/main/java/ch/bbw/CalculatorVisibilityTest.java
+++ src/main/java/ch/bbw/CalculatorVisibilityTest.java
@@ -1,12 +1,44 @@
 package ch.bbw;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorVisibilityTest {
-  @Test
-  public void testZweiPositiveMultiplikationIsOk() {
-    Calculator calculator = new Calculator();
-    assertTrue(calculator.multiplication(3, 4) == 12);
+  private Calculator testee;
+
+  @Before
+  public void setUp() {
+    testee = new Calculator();
   }
+
+  //Multiplication-method tests
+  //====================================================================================================================
+  @Test
+  public void testMultiplicationTwoPositiveIsOk() {
+    assertEquals(10, testee.multiplication(2, 5));
+  }
+
+  @Test
+  public void testMultiplicationTwoNegativeIsOk() {
+    assertEquals(40, testee.multiplication(-20, -2));
+  }
+
+  @Test
+  public void testMultiplicationFirstNumberIsPositiveSecondNumberIsNegativeIsOk() {
+    assertEquals(-20, testee.multiplication(5, -4));
+  }
+
+  @Test
+  public void testMultiplicationFirstNumberIsNegativeSecondNumberPositiveIsOk() {
+    assertEquals(-36, testee.multiplication(-12, 3));
+  }
+
+  @Test
+  public void testMultiplicationResultIsNullIsOk() {
+    assertEquals(0, testee.multiplication(0, 50));
+  }
+  //====================================================================================================================
 }
